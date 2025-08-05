074b1587b5c8ff63463ccea4b33b3b6f06531497
fuwuffy <fuwuffyderg@proton.me>
2025-08-05 10:49:52 +0200

    Base build setup

---
+++ shell.nix
@@ -0,0 +1,49 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+   buildInputs = with pkgs; [
+      # Build tools
+      cmake
+      pkg-config
+      clang_21
+      # OpenGL
+      libGL
+      # Vulkan
+      vulkan-loader
+      vulkan-headers
+      vulkan-validation-layers
+      vulkan-tools
+      # Window system libraries
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXinerama
+      xorg.libXi
+      wayland
+      wayland-scanner
+      wayland-protocols
+      libxkbcommon
+      # Shader tools
+      spirv-tools
+      spirv-headers
+      glslang
+      # Debugging
+      renderdoc
+      valgrind
+      gdb
+   ];
+
+   # Graphics drivers and additional libraries
+   LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXinerama
+      xorg.libXi
+      wayland
+      libxkbcommon
+      libGL
+      vulkan-loader
+   ];
+}
+
